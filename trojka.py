9aacb1dc8cdb6f0c960241af56a88b3a42b43a9f
Filip Zadražil <filipzadrazil@seznam.cz>
2021-02-14 02:00:04 +0100

    1. oprava kódu

--- trojka.py
+++ trojka.py
@@ -1,14 +1,14 @@
-# Nalezení nejkratšího a nejdelšího slova v textu
-# Filip Zadražil, 3. ročník, BGEKA
-# zimní semestr 2020/21
+# Finding shortest and longest word in a text
+# Filip Zadražil, 3. BGEKA
+# winter semester 2020/21
 # Úvod do programování MZ370P19
 
 import os 
 
-# Funkce otevírající příslušný soubor a ošetřující proti nežádoucím vstupům
+# Function loading the picked file and checking invalid inputs
 def load_file(file_name):
     try:
-        with open(file_name, encoding = 'utf-8') as f: # Otevření a dekódování jazyka zkoumaného souboru
+        with open(file_name, encoding = 'utf-8') as f: # Opening and encoding picked file
             data = f.read()
             if os.path.getsize(file_name) == 0:
                 print(f"Soubor {file_name} je prázdný.")
@@ -23,50 +23,59 @@ def load_file(file_name):
         exit()  
     return data
 
-# Funkce hledající v textu nejkratší a nejdelší slova
+# Function searching for the shortest and longest word in a text
 def search_file(file_text):
-    nejdelsi = ""
-    nejkratsi = ""
-    nejdelsi_delka = -1
-    nejkratsi_delka = -1
+    words = file_text.split() # Splitting text to single words
 
-    slova = file_text.split() # Rozdělení textu na jednotlivá slova
-    for i in slova: # Cyklus procházející slovo po slově 
-        delka = len(i)
-        if(i[delka-1] == ',' or i[delka-1] == "." or i[delka-1] == ":"): # Odstranění čárek, teček a dvojteček pro počítání délky slov
-            i = i[:-1]
-            delka = len(i)
-        if(nejdelsi_delka == -1):
-            nejdelsi_delka = delka
-            nejdelsi = i
-        if(nejkratsi_delka == -1):
-            nejkratsi_delka = delka
-            nejkratsi = i
-        if(nejdelsi_delka == delka): 
-            if i in nejdelsi: # Podmínka ošetřující stejně dlouhá slova a případnou duplikaci
+    longest = words[0]
+    shortest = words[0]
+    longest_length = len(words[0])
+    shortest_length = len(words[0])
+
+    for word in words: # Loop checking word by word 
+        i = len(word)
+
+        if(word[i-1] == ',' or word[i-1] == "." or word[i-1] == ":"): # Removing commas, dots and colons at the end of the words
+            word = word[:-1]
+            i = i-1
+        
+        if(longest_length < i): # Overwriting the longest word, if it is longer than the current one
+            longest_length = i
+            longest = word
+
+        if(shortest_length > i):  # Overwriting the shortest word, if it is shorter than the current one
+            shortest_length = i
+            shortest = word
+
+        if(i == longest_length): # Same length requirement
+            if word == longest: # Requirement excluding duplicates
                 pass
             else:
-                nejdelsi = nejdelsi + ", " + i 
-        if(nejkratsi_delka == delka): 
-            if i in nejkratsi: # Podmínka ošetřující stejně dlouhá slova a případnou duplikaci
+                longest = longest + ", " + word # Including the non-duplicate same length word
+        if(i == shortest_length): # Same length requirement
+            if word == shortest: # Requirement excluding duplicates
                 pass
             else:
-                nejkratsi = nejkratsi + ", " + i
-        if(nejdelsi_delka < delka):
-            nejdelsi_delka = delka
-            nejdelsi = i
-        if(nejkratsi_delka > delka):
-            nejkratsi_delka = delka
-            nejkratsi = i
-    longest_word = "Nejdelší slovo/slova v textu je/jsou: " + nejdelsi + " (s délkou " + str(nejdelsi_delka) + ")."
-    shortest_word = "Nejkratší slovo/slova v textu je/jsou: " + nejkratsi + " (s délkou " + str(nejkratsi_delka) + ")."
-    outfile = open('result.txt', 'w', encoding = 'utf-8') # Vytvoření nového txt souboru a uložení výsledků do něj
+                shortest = shortest + ", " + word # Including the non-duplicate same length word
+
+    # Result excluding 'word/words' and 'is/are' in the output 
+    if "," in longest:
+        longest_word = "Longest words in the text are: " + longest + " (" + str(longest_length) + " letter/s long)."
+    else:
+        longest_word = "Longest word in the text is: " + longest + "(" + str(longest_length) + "letter/s long)."
+
+    if "," in shortest:
+        shortest_word = "Shortest words in the text are: " + shortest + " (" + str(shortest_length) + " letter/s long)."
+    else:
+        shortest_word = "Shortest word in the text is: " + shortest + " (" + str(shortest_length) + " letter/s long)."
+
+    outfile = open('result.txt', 'w', encoding = 'utf-8') # Creating a new txt file and saving the results into it
     outfile.write(longest_word + '\n')
     outfile.write(shortest_word)
     outfile.close()
     return longest_word, shortest_word
 
-file_text = load_file("input_file.txt") # Spuštění funkce otevírající soubor, v němž hledáme nejkratší a nejdelší slovo
-longest_word = search_file(file_text) # Spuštění funkce hledající nejkratší a nejdelší slovo
-shortest_word = search_file(file_text) # Spuštění funkce hledající nejkratší a nejdelší slovo
+file_text = load_file("input_file.txt") # Running the function opening the file, in which we want to find the shortest and longest word
+longest_word = search_file(file_text) # Running the function searching for the longest word
+shortest_word = search_file(file_text) # Running the function searching for the shortest word
 
